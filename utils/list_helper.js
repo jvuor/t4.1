cb5017b9bbb31e6c84b244f2052c4293131b1e0a
jvuor <jussi.vuorinen@gmail.com>
2018-06-20 22:03:22 +0300

    more refactoring

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,19 +12,62 @@ const totalLikes = (blogs) => {
 const favoriteBlog = (blogs) => {
 
     const mostLiked = (result, item) => {
-        if (result === undefined) {
-            result = item
-        } else {
-            if (item.likes > result.likes) {
-                result = item
-            }
-        }
+        if (result === undefined || item.likes > result.likes) { result = item }
         return result
     }
 
     return blogs.reduce(mostLiked, undefined)
 }
 
+const mostBlogs = (blogs) => {
+
+    var blogCount = []
+
+    blogs.forEach(blog => {
+
+        var matchingAuthor = blogCount.findIndex(x => x.author === blog.author)
+
+        if (matchingAuthor === -1) {
+            blogCount.push({
+                author: blog.author,
+                count: 1
+            })
+        } else {
+            blogCount[matchingAuthor].count += 1
+        }
+                
+    });
+
+    return blogCount
+
+}
+
+const mostLikes = (blogs) => {
+    var likeCount = []
+
+    blogs.forEach(blog => {
+
+        var matchingAuthor = likeCount.findIndex(x => x.author === blog.author)
+
+        if (matchingAuthor === -1) {
+            likeCount.push({
+                author: blog.author,
+                likes: blog.likes
+            })
+        } else {
+            likeCount[matchingAuthor].likes += blog.likes
+        }
+                
+    })
+
+    var topLikes = (result, item) => {
+        if (result === undefined || item.likes > result.likes) { result = item }
+        return result
+    }
+
+    return likeCount.reduce(topLikes, undefined)
+}
+
 module.exports = {
-    dummy, totalLikes, favoriteBlog
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
