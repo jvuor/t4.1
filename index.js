5d9f3210df2e7d3961356ced3a52543ec264290a
jvuor <jussi.vuorinen@gmail.com>
2018-06-28 22:09:47 +0300

    token-based authentication (4.18)

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 
 const mongoUrl = config.mongoUrl
@@ -24,6 +25,7 @@ app.use(bodyParser.json())
 app.use(middleware.logger)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.error)
 
