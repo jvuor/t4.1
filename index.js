4859a8b7d6d9c8e0547663ee8d52e87499236d6f
jvuor <jussi.vuorinen@gmail.com>
2018-07-09 19:05:25 +0300

    bug fixes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use('/users', express.static('build'))
-app.use(express.static('build'))
+app.use('*', express.static('build'))
 
 app.use(middleware.error)
 
